317bd4982bb6ece6d860386321ad43f726478663
Qais Wardag <qais.wardag@outlook.com>
2025-08-14 01:07:13 +0200

    feat: Update authorization logic in request classes to allow user access

--- app/Http/Requests/UpdateBuildingRequest.php
+++ app/Http/Requests/UpdateBuildingRequest.php
@@ -11,7 +11,7 @@ class UpdateBuildingRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
--- app/Http/Requests/UpdateTenancyPeriodRequest.php
+++ app/Http/Requests/UpdateTenancyPeriodRequest.php
@@ -11,7 +11,7 @@ class UpdateTenancyPeriodRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
