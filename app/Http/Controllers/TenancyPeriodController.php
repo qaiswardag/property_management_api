260aca8d5741407de5984896326228833db96f95
Qais Wardag <qais.wardag@outlook.com>
2025-08-13 19:10:37 +0200

    feat: Add controllers, requests, models, policies, factories, migrations, and seeders for property management system

---
+++ app/Http/Controllers/TenancyPeriodController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTenancyPeriodRequest;
+use App\Http\Requests\UpdateTenancyPeriodRequest;
+use App\Models\TenancyPeriod;
+
+class TenancyPeriodController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTenancyPeriodRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TenancyPeriod $tenancyPeriod)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TenancyPeriod $tenancyPeriod)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTenancyPeriodRequest $request, TenancyPeriod $tenancyPeriod)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TenancyPeriod $tenancyPeriod)
+    {
+        //
+    }
+}
---
+++ app/Models/TenancyPeriod.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TenancyPeriod extends Model
+{
+    /** @use HasFactory<\Database\Factories\TenancyPeriodFactory> */
+    use HasFactory;
+}
