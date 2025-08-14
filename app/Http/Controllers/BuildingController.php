504a0cb4e0271d1f31b20a4fb3a9e40cf5f54576
Qais Wardag <qais.wardag@outlook.com>
2025-08-14 07:50:03 +0200

    feat: Enhance BuildingController with validation and JSON responses for resource management

--- app/Http/Controllers/BuildingController.php
+++ app/Http/Controllers/BuildingController.php
@@ -5,17 +5,19 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreBuildingRequest;
 use App\Http\Requests\UpdateBuildingRequest;
 use App\Models\Building;
+use App\Models\Corporation;
+use Illuminate\Http\Request;
 
 class BuildingController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $request->validate(['corporation_id' => 'required|exists:corporations,id']);
+        return Building::where('corporation_id', $request->corporation_id)->get();
     }
-
     /**
      * Show the form for creating a new resource.
      */
@@ -29,7 +31,21 @@ class BuildingController extends Controller
      */
     public function store(StoreBuildingRequest $request)
     {
-        //
+        $request->validate([
+            'name' => 'required|string',
+            'corporation_id' => 'required|exists:corporations,id',
+            'zip_code' => 'required|string',
+        ]);
+
+        $corporation = Corporation::find($request->corporation_id);
+
+        return Building::create([
+            'name' => $request->name,
+            'corporation_id' => $corporation->id,
+            'height' => $corporation->height + 1,
+            'zip_code' => $request->zip_code,
+            'type' => 'Building',
+        ]);
     }
 
     /**
@@ -37,7 +53,7 @@ class BuildingController extends Controller
      */
     public function show(Building $building)
     {
-        //
+        return $building;
     }
 
     /**
@@ -53,7 +69,19 @@ class BuildingController extends Controller
      */
     public function update(UpdateBuildingRequest $request, Building $building)
     {
-        //
+        $request->validate([
+            'name' => 'string',
+            'corporation_id' => 'exists:corporations,id',
+            'zip_code' => 'string',
+        ]);
+
+        if ($request->corporation_id) {
+            $building->corporation_id = $request->corporation_id;
+            $building->height = Corporation::find($request->corporation_id)->height + 1;
+        }
+
+        $building->update($request->only('name', 'zip_code'));
+        return $building;
     }
 
     /**
@@ -61,6 +89,11 @@ class BuildingController extends Controller
      */
     public function destroy(Building $building)
     {
-        //
+        $building->delete();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Building deleted successfully.'
+        ]);
     }
 }
